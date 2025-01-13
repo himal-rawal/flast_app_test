7857ecd879984e33ee92b79474e6bdcf57ef8bef
Himal <himal.rawal@8squarei.com>
2025-01-13 18:58:04 +0545

    fix: add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
+from flask_cors import CORS
 from flask import Flask
 from .dynamic_api import dynamic_api
-
 def create_app():
     app = Flask(__name__)
-
-    # Attach the app instance to the blueprint
-    dynamic_api.app = app
-
-    # Register the blueprint
+    CORS(app)
     app.register_blueprint(dynamic_api)
-
     return app
