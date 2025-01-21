4003a54992172b652290923d4f982ec4d8d14f5e
Himal <himal.rawal@8squarei.com>
2025-01-22 00:54:20 +0545

    add paste text

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    
+    '''
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('DB_HOSTNAME')}/{os.getenv('DB_NAME')}"
     )
+    '''
 
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
